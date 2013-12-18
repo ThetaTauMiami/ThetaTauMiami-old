89e960cb331a3b9063c2297a43aa22083f51c419
rogerskw <rogerskw@miamioh.edu>
2013-12-18 14:52:12 -0500

    Migrated information models to info

--- ThetaTauMiami/urls.py
+++ ThetaTauMiami/urls.py
@@ -5,7 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'ThetaTauMiami.views.frontpage', name='frontpage'),
+    url(r'^$', 'ThetaTauMiami.views.marketing', name='index'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
--- marketing/admin.py
+++ marketing/admin.py
@@ -1,11 +1,2 @@
 from django.contrib import admin
 
-from marketing.models import PledgeClass, Brother, Department, Major, HasMajor
-
-# Register your models here.
-
-admin.site.register(Brother)
-admin.site.register(PledgeClass)
-admin.site.register(Department)
-admin.site.register(Major)
-admin.site.register(HasMajor)
