7fa48531a1e154af4e56987b3b5163368c4422ca
rogerskw <rogerskw@miamioh.edu>
2013-12-18 22:57:16 -0500

    more stuff

--- ThetaTauMiami/settings.py
+++ ThetaTauMiami/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = 'q=asy$r^!q72tp@x2f6kxg83d)+@mg59u0v8#pby^bv(=n4__0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-TEMPLATE_DEBUG = False
+TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', 'aqueous-ravine-6084.herokuapp.com']
 
