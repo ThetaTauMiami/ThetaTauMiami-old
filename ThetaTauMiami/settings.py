c43fa6de5034a78a6be26d0a8afdce90878896c0
rogerskw <rogerskw@miamioh.edu>
2013-12-19 21:03:45 -0500

    more production tests

--- ThetaTauMiami/settings.py
+++ ThetaTauMiami/settings.py
@@ -66,7 +66,7 @@ WSGI_APPLICATION = 'ThetaTauMiami.wsgi.application'
 DATABASES = dict()
 
 # Production setting
-#DATABASES['default'] = dj_database_url.config()
+DATABASES['default'] = dj_database_url.config()
 
 # Local setting
 DATABASES['default'] = { "ENGINE" : 'django.db.backends.sqlite3', "NAME": os.path.join(BASE_DIR, 'db.sqlite3') }
