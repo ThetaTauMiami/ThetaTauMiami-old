d013a356be71aae1ffbe82589681e20487e3c6d9
rogerskw <rogerskw@miamioh.edu>
2014-01-03 20:58:23 -0500

    Fixed a stupid bug that happens on the last page during brothers list pages.

--- info/views.py
+++ info/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+import math
 
 from django.http import HttpResponse
 from django.template import Context, loader
@@ -42,7 +42,7 @@ def general_listing(request, isAlumniFilter, isPledgeFilter, name):
     brothers_range_max = (page_number) * brothers_count
     brothers = Brother.objects.filter(isAlumni=isAlumniFilter, isPledge=isPledgeFilter).order_by('lastName', 'firstName', 'middleName')
     number_of_brothers = len(brothers)
-    total_pages = (number_of_brothers / brothers_count) + 1
+    total_pages = int(math.ceil(number_of_brothers / float(brothers_count)))
     brothers = brothers[brothers_range_min:brothers_range_max]
     brother_list_list = utility.convert_array_to_Yx3(brothers) if len(brothers) > 0 else None
     page_numbers_list = calculate_page_range(total_pages, page_number)
@@ -77,7 +77,7 @@ def get_page_number(request):
 
 def calculate_page_range(total_pages, page_number):
     max_pages_listed_on_screen = 5
-    if total_pages == page_number: # If there is only the one page, there is no need to display page numbers
+    if total_pages == 1: # If there is only the one page, there is no need to display page numbers
         return []
     elif total_pages <= max_pages_listed_on_screen: # In this case, just display all of the available pages
         min_page_number_displayed = 1
@@ -86,7 +86,7 @@ def calculate_page_range(total_pages, page_number):
         min_page_number_displayed = 1
         max_page_number_displayed = min_page_number_displayed + max_pages_listed_on_screen
     elif page_number + max_pages_listed_on_screen / 2 >= total_pages: # We are near the end. In this case, display from (end - max_pages_listed_on_screen) to end
-        max_page_number_displayed = total_pages
+        max_page_number_displayed = total_pages + 1
         min_page_number_displayed = max_page_number_displayed - max_pages_listed_on_screen
     else: # We are somewhere in the middle. In this case, just display some pages on either side
         min_page_number_displayed = page_number - 2
