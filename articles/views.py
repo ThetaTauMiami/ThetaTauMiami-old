947333c39f41878505b3eb291432e45346c6e3e6
rogerskw <rogerskw@miamioh.edu>
2014-01-08 16:52:26 -0500

    Fixed missing httpresponse

--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, get_object_or_404
+
+from django.http import HttpResponse
 from django.template import Context, loader
 
 from articles.models import Article, ArticleEntity
